21da21d25f91ab424ec04db9a99468fd92cf6aaa
Алина Колодина <a.kolodina@g.nsu.ru>
2025-12-04 15:47:06 +0700

    wrote service and controller

--- src/main/java/nsu/library/repository/ReadLaterRepository.java
+++ src/main/java/nsu/library/repository/ReadLaterRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import nsu.library.entity.ReadLater;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReadLaterRepository extends JpaRepository<ReadLater, ReadLaterId> {
+     List<ReadLater> findByUserId(Long userId);
 }
--- src/main/java/nsu/library/service/books/ReadLaterService.java
+++ src/main/java/nsu/library/service/books/ReadLaterService.java
@@ -1,15 +1,23 @@
 package nsu.library.service.books;
 
 import lombok.RequiredArgsConstructor;
+import nsu.library.dto.ReadLaterBookDTO;
+import nsu.library.entity.Book;
 import nsu.library.entity.ReadLater;
+import nsu.library.repository.BookRepository;
 import nsu.library.repository.ReadLaterRepository;
 import nsu.library.util.ReadLaterId;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReadLaterService {
     private final ReadLaterRepository readLaterRepository;
+    private final BookRepository bookRepository;
+    private final BookService bookService;
 
     public ReadLater addBookToReadLater(Long userId, Long bookId) {
         ReadLater readLater = new ReadLater();
@@ -18,10 +26,26 @@ public class ReadLaterService {
         return readLaterRepository.save(readLater);
     }
 
-    public void removeBookFromReadLater(Long userId, Long bookId) {
+    public void deleteBookFromReadLater(Long userId, Long bookId) {
         ReadLaterId id = new ReadLaterId();
         id.setUserId(userId);
         id.setBookId(bookId);
         readLaterRepository.deleteById(id);
+    }
+
+    public List<ReadLaterBookDTO> getListOfReadLaterBooksByUser(Long userId) {
+        List<ReadLater> readLaterList = readLaterRepository.findByUserId(userId);
+        List<ReadLaterBookDTO> laterBookDTOList = new ArrayList<>();
+        for (ReadLater readLater : readLaterList) {
+            Book book = bookRepository.findById(readLater.getBookId()).orElse(null);
+            if (book != null) {
+                ReadLaterBookDTO readLaterBookDTO = new ReadLaterBookDTO();
+                readLaterBookDTO.setBookId(book.getId());
+                readLaterBookDTO.setUserId(readLater.getUserId());
+                readLaterBookDTO.setBook(bookService.convertBookToDTO(book));
+                laterBookDTOList.add(readLaterBookDTO);
+            }
+        }
+        return laterBookDTOList;
     }
 }
