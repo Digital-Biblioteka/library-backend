4f48af48a23311be9cf216ac5aa73f428e23da9c
Алина Колодина <a.kolodina@g.nsu.ru>
2025-12-10 13:11:39 +0700

    return only link to cover in getPreview

--- src/main/java/nsu/library/service/books/ReaderService.java
+++ src/main/java/nsu/library/service/books/ReaderService.java
@@ -1,8 +1,6 @@
 package nsu.library.service.books;
 
 import lombok.RequiredArgsConstructor;
-import nsu.library.dto.BookDTO;
-import nsu.library.dto.BookPreviewDTO;
 import nsu.library.entity.Book;
 import nsu.library.repository.BookRepository;
 import nsu.library.service.minio.MinioService;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Service
 @RequiredArgsConstructor
 public class ReaderService {
-    private final BookService bookService;
     private final BookRepository bookRepository;
     private final MinioService minioService;
 
@@ -23,15 +20,9 @@ public class ReaderService {
      * @param bookId ид книжки
      * @return dto
      */
-    public BookPreviewDTO getBookPreview(@PathVariable Long bookId) {
+    public String getBookPreview(@PathVariable Long bookId) {
         Book book = bookRepository.findById(bookId).orElseThrow();
-        BookDTO metadata = bookService.convertBookToDTO(book);
         String bookLink = book.getLinkToBook();
-        String coverLink = minioService.getBookCover(bookLink);
-
-        BookPreviewDTO bookPreviewDTO = new BookPreviewDTO();
-        bookPreviewDTO.setCover(coverLink);
-        bookPreviewDTO.setMetadata(metadata);
-        return bookPreviewDTO;
+        return minioService.getBookCover(bookLink);
     }
 }
